5792212917a237778cc0b6c362c506afb4b2b166
franzgeraldroque <franzgeraldroque17@gmail.com>
2025-08-12 22:35:00 +0800

    code-challenge1.py for 1st Python challenge activity

---
+++ code-challenge1.py
@@ -0,0 +1,3 @@
+name = input("What is your name?")
+
+print("\t\t\t\t\t♦ \n\n\n \t\t\t\t♦\t\t♦ \n\n\n \t\t\t♦\t\t\t\t♦ \n\n\n \t\t♦\t\t\tHI\t\t\t♦ \n\n\n \t♦\t\t\t", name , "\t\t\t♦ \n\n\n \t\t♦\t\t\t\t\t\t♦ \n\n\n \t\t\t♦\t\t\t\t♦ \n\n\n \t\t\t\t♦\t\t♦ \n\n\n \t\t\t\t\t♦")
