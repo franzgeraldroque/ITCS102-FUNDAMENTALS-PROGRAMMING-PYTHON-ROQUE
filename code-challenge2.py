22d206b4b39b99c8c0fab96ca4b81dc9a4fc6c74
franzgeraldroque <franzgeraldroque17@gmail.com>
2025-08-15 17:09:52 +0800

    This is my 2nd code challenge

--- code-challenge2.py
+++ code-challenge2.py
@@ -2,7 +2,6 @@ amount = eval(input("Enter amount to deposit :"))
 
 denominations = [1000, 500, 200, 100, 50, 20, 10, 5, 1]
 
-
 print("\nHere is a breakdown in PH denomination: ")
 
 for denom in denominations:
