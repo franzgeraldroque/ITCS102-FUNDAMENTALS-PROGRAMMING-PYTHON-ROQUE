171a28143bf55055cbd421553df7cbb6705707fc
franzgeraldroque <franzgeraldroque17@gmail.com>
2025-08-15 16:41:40 +0800

    This is my 4th python act.

--- activity1.py
+++ activity1.py
@@ -1,1 +1,1 @@
-print("Hello World")
+print("Hello Hanniel my BFF")
---
+++ code-challenge2.py
@@ -0,0 +1,15 @@
+amount = eval(input("Enter amount to deposit :"))
+
+denominations = [1000, 500, 200, 100, 50, 20, 10, 5, 1]
+
+
+print("\nHere is a breakdown in PH denomination: ")
+
+for denom in denominations:
+    count = amount // denom
+    amount = amount % denom
+    print(count, "-", denom)
+
+text = input("\nSana lahat ng tao ganito: ")
+
+
