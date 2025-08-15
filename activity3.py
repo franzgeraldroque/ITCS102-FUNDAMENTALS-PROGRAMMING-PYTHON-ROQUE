4ea94fa3a0115ea112d5671a9c8fc3f94c19b810
franzgeraldroque <franzgeraldroque17@gmail.com>
2025-08-15 17:35:52 +0800

    This is my 3rd python act

--- activity3.py
+++ activity3.py
@@ -1,4 +1,4 @@
 print("Happy\n\tMonday,")
 print("BSIT 1A\n\t from DLL")
 print("Hi ex, \n\t kakalimutan na kita")
-print("The quick snatcher runs\n\t over\n\t manila city")
+print("The quick snatcher runs\n\t over\n\t manila city") 
