3f1b4ccf2c39d56e9be20703865338c745f35f67
franzgeraldroque <franzgeraldroque17@gmail.com>
2025-08-23 12:54:36 +0800

    This is my 10th python act

---
+++ activity10.py
@@ -0,0 +1,12 @@
+#tricycle fare
+
+name = input("Input your name ---> ")
+isStudent = input("Are you a student (Yes / No) --> ")
+fare = eval(input("bayad --> "))
+
+if isStudent.lower() == "yes":
+	discount = fare * .20
+	new_fare = fare - discount
+	print("Hi", name, " student discount is ", discount, " Discounted fare is ", new_fare)
+else:
+	print("Sorry", name, " you are not eligible for student discount")
