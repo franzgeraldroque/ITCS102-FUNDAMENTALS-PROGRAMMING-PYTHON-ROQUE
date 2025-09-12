308280a23056f1889922bb6fadb03c67d47c0211
franzgeraldroque <franzgeraldroque17@gmail.com>
2025-09-12 11:24:10 +0800

    This is my 13th python act

---
+++ activity13.py
@@ -0,0 +1,8 @@
+#using for loop, ask user to input 10 numbers
+#after input, get the summation of all the numbers
+num = 0
+for cashg in range(1, 11):
+    number = eval(input('Enter a number --> '))
+    num += number
+    print('ang laman ng imong gcash ay',num)
+print("ang imong gcash ay may laman na",num, "pidi ba akong makikurot kahit pang lud lang")
