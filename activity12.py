25f5b5b18240487c09e3fe5b3c386a43ec02add5
franzgeraldroque <franzgeraldroque17@gmail.com>
2025-09-12 11:23:46 +0800

    This is my 12th python act

---
+++ activity12.py
@@ -0,0 +1,7 @@
+#print("Hello World")
+#name = input("Enter your name: ")
+#print("Hello", name, "say Hi to your future girlfriend")
+
+#print hello world 10 times
+for truepa in range(1, 100, 2):
+    print(truepa, "- kamusta sa aking magiting na kaibigan")
