eec93da48b4815900a11b28fd293d6c9d0f98d45
franzgeraldroque <franzgeraldroque17@gmail.com>
2025-09-15 13:23:36 +0800

    This is my 7th code challenge

--- code-challenge7.py
+++ code-challenge7.py
@@ -1,11 +1,12 @@
 print("ODD NUMBER SUMMATION")
-print("Maglagay ng sampong numero")
+print("Maglagay ng sampung numero")
 
 odd = 0
 for i in range(10):
-    number = eval(input("ilagay mo ang numero -->"))
+    number = eval(input("ilagay mo ang numero beybe -->"))
     if number %2 != 0:
         odd += number
     else:
         print("ang numero", number, "ay hindi odd number")
+
 print("ang kabuuan ng pangkalahatang odd number ay", odd)
