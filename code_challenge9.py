05f9121e367ce21b13ec32999f6b9d2e7416337f
franzgeraldroque <franzgeraldroque17@gmail.com>
2025-09-17 10:51:44 +0800

    This is my 9th code challenge

---
+++ code_challenge9.py
@@ -0,0 +1,8 @@
+import time
+print("COUNTDOWN TIMER SIMULATOR")
+num = int(input("Enter a number --> "))
+print("Countdown started: ")
+for i in range(num, 0, -1):
+    print(i)
+    time.sleep(1)
+print("TAKBO KAIBIGAN!!")
