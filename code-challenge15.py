0f872eb11b31c17f37cf0e4abc5cc9a7ec89a177
franzgeraldroque <franzgeraldroque17@gmail.com>
2025-10-22 12:04:39 +0800

    This is my 15th code challenge

--- code-challenge15.py
+++ code-challenge15.py
@@ -1,5 +1,3 @@
-# # title = input("Enter the title of the anime (or type 'stop' to finish: )")
-
 anim = ["Jujutsu Kaisen", "Naruto", "Attack on Titan", "MyHeroAcademia"]
 anime = True
 anime_lower = [anim.lower for anim in anim]
@@ -20,29 +18,6 @@ while anime == True:
         continue
 
 # anim = ["Jujutsu Kaisen", "Naruto", "Attack on Titan", "MyHeroAcademia"]
-# anime = True
-
-# # Convert all anime titles to lowercase for comparison
-# anim_lower = [a.lower() for a in anim]
-
-# while anime:
-#     title = input("Enter the title of the anime (or type 'stop' to finish): ").strip().lower()
-
-#     if title == 'stop':
-#         print('You have stopped the anime recommendation list.')
-#         break
-
-#     elif title in anim_lower:
-#         # Find the original title with correct casing
-#         original_title = anim[anim_lower.index(title)]
-#         print(f"'{original_title}' has been added to your anime watch list.")
-#         continue
-
-#     else:
-#         print("No such anime is found in the given anime recommendations.")
-#         continue
-
-
-# anim = ["Jujutsu Kaisen", "Naruto", "Attack on Titan", "MyHeroAcademia"]
 # for anime in anime:
+
 #     print(anime)
