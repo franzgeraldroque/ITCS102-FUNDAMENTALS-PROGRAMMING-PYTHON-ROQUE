5d738e90163f0b7235ca17af9600a0870e8f6eb5
franzgeraldroque <franzgeraldroque17@gmail.com>
2025-11-14 11:27:30 +0800

    This is my 25_1 python act

---
+++ activity25_1.py
@@ -0,0 +1,14 @@
+def activity1():
+    a = 5
+    print(f'The value of a is, {a}')
+
+def activity2():
+    name = input('What is your name?')
+    print(f'Hi, {name}, Welcome to the Matrix')
+
+def activity3():
+    anim = input("Hi, enter your favorite anime --> ")
+    print(f'The anime, {anim}, is a good choice')
+
+def activity4():
+    print("Hi,\n Hanniel, \n my bff")
